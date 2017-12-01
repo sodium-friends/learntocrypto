ea056aa4bfbed6bd12ef52c4b0b329f4a173a2ac
Luiz Eduardo <dudu.christo@gmail.com>
2017-12-01 19:32:23 +0100

    Implements withdraw with founds check

---
+++ solutions/03/teller.js
@@ -0,0 +1,32 @@
+var jsonStream = require('duplex-json-stream');
+var net = require('net');
+
+var client = jsonStream(net.connect(3876));
+var argv = process.argv.slice(2);
+
+var command = argv[0];
+
+client.on('data', function (msg) {
+    console.log('Teller received:', msg)
+});
+
+switch (command) {
+
+    case 'deposit':
+        var amount = parseFloat(argv[1]);
+        client.end({cmd: 'deposit', amount: amount});
+        break;
+
+    case 'withdraw':
+        var amount = parseFloat(argv[1])*(-1);
+        client.end({cmd: 'withdraw', amount: amount});
+        break;
+
+    case 'balance':
+        client.end({cmd: 'balance'});
+        break;
+
+    case 'help':
+    default:
+        console.log('node teller.js [CMD]')
+}
